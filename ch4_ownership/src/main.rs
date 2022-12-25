31cfa555c4badbed86bcfc7521335a95be90e58b
gainchang <gainchang620@gmail.com>
2022-12-26 04:14:22 +0900

    Add ownership related study codes

---
+++ ch4_ownership/src/main.rs
@@ -0,0 +1,24 @@
+mod r#move;
+mod move2;
+
+fn main() {
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    // borrow of moved value: `s1` [E0382]
+    // println!("{}, world!", s1);
+
+    let s3 = String::from("hello");
+    let s4 = s3.clone();
+
+    println!("s3 = {}, s4 = {}", s3, s4);
+
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+
+    r#move::main2();
+    move2::main3();
+}
